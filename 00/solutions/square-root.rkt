cc604580c8660c418bbd952645591ca47f38d0ad
Deian Goranov <deian.goranov@gmail.com>
2019-11-05 01:05:52 +0200

    Add solutions for homework 0

--- 00/solutions/square-root.rkt
+++ 00/solutions/square-root.rkt
@@ -1,5 +1,19 @@
 #lang racket
 
+(require math/flonum)
+
 (provide my-sqrt)
 
-(define (my-sqrt x) void)
+(define (iterate n f x)
+  (if (= n 0)
+      x
+      (f (iterate (- n 1) f x))))
+
+(define (square-root-with-precision n)
+  (lambda (x)
+    (define (reinforce y)
+       (* 0.5 (+ y (/ x y))))
+    (iterate n reinforce (fllog2 (fl x)))))
+
+; 8 са най-малкото стъпки, за които минават тестовете
+(define my-sqrt (square-root-with-precision 8))
