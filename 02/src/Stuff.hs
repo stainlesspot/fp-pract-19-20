82889b70f8d7ba94dffe1b57d6db00ffee16b03c
Deian Goranov <deian.goranov@gmail.com>
2019-12-21 17:31:36 +0200

    Add solutions to non-bonus tasks

--- 02/src/Stuff.hs
+++ 02/src/Stuff.hs
@@ -9,30 +9,53 @@ module Stuff
   , on
   ) where
 
-group :: Eq a => [a] -> [[a]]
-group = undefined
+import Data.List (partition)
 
--- Not mandatory, delete if you don't want this.
+group :: Eq a => [a] -> [[a]]
+group = groupBy (==)
+
 insertBy :: (a -> a -> Ordering) -> a -> [a] -> [a]
-insertBy = undefined
+insertBy _ x []     = [x] 
+insertBy f x (y:ys) = if f x y == LT
+                      then x:y:ys
+                      else y : insertBy f x ys
 
 sortBy :: (a -> a -> Ordering) -> [a] -> [a]
-sortBy = undefined
+sortBy _ [] = []
+sortBy f (x:xs) = insertBy f x $ sortBy f xs
 
 groupBy :: (a -> a -> Bool) -> [a] -> [[a]]
-groupBy = undefined
+groupBy f = foldr insertGroup []
+  where insertGroup x [] = [[x]]
+        insertGroup x ([]:zss) = [x]:zss -- should never happen
+        insertGroup x rec@((y:ys):zss) =
+          if f x y
+          then (x:y:ys):zss
+          else [x]:rec
 
 on :: (b -> b -> c) -> (a -> b) -> a -> a -> c
-on = undefined
+on g f x y = g (f x) (f y)
 
 (&&&) :: (a -> b) -> (a -> c) -> a -> (b, c)
-(&&&) = undefined
+(&&&) f g x = (f x,g x)
 
 sortOn :: Ord b => (a -> b) -> [a] -> [a]
-sortOn = undefined
+sortOn f = map fst . sortBy (compare `on` snd) . map (id &&& f)
 
 groupOn :: Eq b => (a -> b) -> [a] -> [[a]]
-groupOn = undefined
+groupOn f = map (map fst) . groupBy ((==) `on` snd) . map (id &&& f)
 
-classifyOn :: Ord b => (a -> b) -> [a] -> [[a]]
-classifyOn = undefined
+classifyOn :: Eq b => (a -> b) -> [a] -> [[a]]
+classifyOn f = map (map fst) . classifyBy ((==) `on` snd) . map (id &&& f)
+
+classifyBy :: (a -> a -> Bool) -> [a] -> [[a]]
+classifyBy _    []     = []
+classifyBy (~~) (x:xs) = (x:eqs) : classifyBy (~~) others
+  where (eqs,others) = partition (~~ x) xs
+
+-- Ако искаш да имплементирам сам partition
+--partition :: (a -> Bool) -> [a] -> ([a],[a])
+--partition p = foldr place ([],[])
+--  where place x (ts,fs) = if p x
+--                          then (x:ts,fs)
+--                          else (ts,x:fs)
