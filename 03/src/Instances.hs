f85b04c32eb935a21f120079f0a41654f0d73f3f
Deian Goranov <deian.goranov@gmail.com>
2020-01-08 00:11:28 +0200

    Add hw3 solutions

--- 03/src/Instances.hs
+++ 03/src/Instances.hs
@@ -7,13 +7,16 @@ import Prelude hiding (reverse)
 
 import Data.Char (isSpace)
 import Data.Function (on)
+import Control.Monad (liftM2)
+import Data.List (foldl')
 
 newtype Pointwise a b = Pointwise {getPointwise :: (a, b)}
   deriving (Show, Eq)
 
 instance (Ord a, Ord b) => Ord (Pointwise a b) where
   (<=) :: Pointwise a b -> Pointwise a b -> Bool
-  (<=) = undefined
+  Pointwise (x1, y1) <= Pointwise (x2, y2)
+    = x1 <= x2 && y1 <= y2
 
 newtype Lexicographic a b = Lexicographic {getLexicographic :: (a, b)}
   deriving (Show, Eq)
@@ -21,65 +24,75 @@ newtype Lexicographic a b = Lexicographic {getLexicographic :: (a, b)}
 -- The default instance for tuples and lists
 instance (Ord a, Ord b) => Ord (Lexicographic a b) where
   (<=) :: Lexicographic a b -> Lexicographic a b -> Bool
-  (<=) = undefined
+  Lexicographic (x1, y1) <= Lexicographic (x2, y2)
+    = x1 < x2 || (x1 == x2 && y1 <= y2)
 
 newtype Fun a b = Fun {getFun :: a -> b}
 
 instance (Semigroup b) => Semigroup (Fun a b) where
   (<>) :: Fun a b -> Fun a b -> Fun a b
-  (<>) = undefined
+  Fun f <> Fun g = Fun $ \x -> f x <> g x
 
 instance (Monoid b) => Monoid (Fun a b) where
   mempty :: Fun a b
-  mempty = undefined
+  mempty = Fun $ const mempty
 
 newtype First a = First {getFirst :: Maybe a}
   deriving (Eq, Show)
 
 instance Semigroup (First a) where
   (<>) :: First a -> First a -> First a
-  (<>) = undefined
+  f@(First (Just _)) <> _ = f
+  _                  <> s = s
 
 instance Monoid (First a) where
   mempty :: First a
-  mempty = undefined
+  mempty = First Nothing
 
 newtype Last a = Last {getLast :: Maybe a}
   deriving (Eq, Show)
 
 instance Semigroup (Last a) where
   (<>) :: Last a -> Last a -> Last a
-  (<>) = undefined
+  _ <> l@(Last (Just _)) = l
+  s <> _                 = s
 
 instance Monoid (Last a) where
   mempty :: Last a
-  mempty = undefined
+  mempty = Last Nothing
 
 newtype Pair a b = Pair {getPair :: (a, b)}
   deriving (Show, Eq)
 
+
 -- The default instance for tuples
 instance (Semigroup a, Semigroup b) => Semigroup (Pair a b) where
   (<>) :: Pair a b -> Pair a b -> Pair a b
-  (<>) = undefined
+  Pair (x1, y1) <> Pair (x2, y2) = Pair (x1 <> x2, y1 <> y2)
 
 instance (Monoid a, Monoid b) => Monoid (Pair a b) where
   mempty :: Pair a b
-  mempty = undefined
+  mempty = Pair (mempty, mempty)
 
 newtype Dual a = Dual {getDual :: a}
   deriving (Show, Eq)
 
 instance Semigroup a => Semigroup (Dual a) where
   (<>) :: Dual a -> Dual a -> Dual a
-  (<>) = undefined
+  Dual x <> Dual y = Dual $ y <> x
 
 instance Monoid a => Monoid (Dual a) where
   mempty :: Dual a
-  mempty = undefined
+  mempty = Dual mempty
 
+-- Of course foldl/foldr/foldMap give different complexity.
+-- foldMap is implemented as foldr, so they are the same, giving O(n^2).
+-- And foldl with (flip (:)) has O(n) complexity, here I just mimic it with Dual
 reverse :: [a] -> [a]
-reverse = undefined
+reverse = getDual . foldl' (\acc x -> acc <> Dual [x]) mempty
+
+-- can't get faster than this
+--reverse = foldl' (flip (:)) []
 
 data Flux a = Flux
   { sides :: Maybe (a, a)
@@ -92,8 +105,13 @@ flux x = Flux (Just (x, x)) 0
 
 instance (Eq a) => Semigroup (Flux a) where
   (<>) :: Flux a -> Flux a -> Flux a
-  (<>) = undefined
+  Flux (Just (x1, y1)) n <> Flux (Just (x2, y2)) m
+    = Flux (Just (x1, y2)) (n + m + extra)
+    where extra = if y1 == x2 then 0 else 1
+  f@(Flux (Just _) _) <> _ = f
+  _ <> s = s
+  
 
 instance (Eq a) => Monoid (Flux a) where
   mempty :: Flux a
-  mempty = undefined
+  mempty = Flux Nothing 0
--- 03/src/Trees.hs
+++ 03/src/Trees.hs
@@ -6,6 +6,7 @@ module Trees where
 import Prelude
 import Data.Monoid (Sum(..), All(..), Any(..), First(..))
 import Data.Maybe (isJust)
+import Data.List (foldl')
 
 data Tree a
   = Empty
@@ -14,63 +15,117 @@ data Tree a
 
 instance Eq a => Eq (Tree a) where
   (==) :: Tree a -> Tree a -> Bool
-  (==) = undefined
+  Empty == Empty = True
+  Node x1 l1 r1 == Node x2 l2 r2
+    =  x1 == x2
+    && l1 == l2
+    && r1 == r2
+  _ == _ = False
+
+leaf :: a -> Tree a
+leaf x = Node x Empty Empty
 
 insertOrdered :: Ord a => a -> Tree a -> Tree a
-insertOrdered = undefined
+insertOrdered x Empty        = leaf x
+insertOrdered x (Node y l r) =
+  if x <= y
+  then Node y (insertOrdered x l) r
+  else Node y l (insertOrdered x r)
 
 listToBST :: Ord a => [a] -> Tree a
-listToBST = undefined
+listToBST = foldl' (flip insertOrdered) Empty
 
 isBST :: Ord a => Tree a -> Bool
-isBST = undefined
+-- first idea
+isBST Empty = True
+isBST (Node x l r) = check (<=) l && check (>) r
+  where check _    Empty          = True
+        check (~~) t@(Node y _ _) = y ~~ x && isBST t
+-- also works
+--isBST = between Bot Top
 
--- idea for implementing isBST - delete if you don't want it
 data BotTop a = Bot | Val a | Top
   deriving (Show, Eq, Ord)
 
 between :: Ord a => BotTop a -> BotTop a -> Tree a -> Bool
-between = undefined
+between low high Empty        = low <= high
+between low high (Node x l r)
+  =  low < vx
+  && vx <= high
+  && between low vx   l
+  && between vx  high r
+  where vx = Val x
 
 findBST :: Ord a => a -> Tree a -> Bool
-findBST = undefined
+findBST _ Empty        = False
+findBST x (Node y l r) = case compare x y of
+  EQ -> True
+  LT -> findBST x l
+  GT -> findBST x r
 
 mapTree :: (a -> b) -> Tree a -> Tree b
-mapTree = undefined
+mapTree _ Empty        = Empty
+mapTree f (Node x l r) = Node (f x) (mapTree f l) (mapTree f r)
 
 foldTree :: Monoid a => Tree a -> a
-foldTree = undefined
+foldTree Empty        = mempty
+foldTree (Node x l r) = foldTree l <> x <> foldTree r
 
 foldMapTree :: Monoid b => (a -> b) -> Tree a -> b
-foldMapTree = undefined
+foldMapTree f = foldTree . mapTree f
 
 sumTree :: Num a => Tree a -> a
-sumTree = undefined
+sumTree = getSum . foldMapTree Sum
 
 allTree :: (a -> Bool) -> Tree a -> Bool
-allTree = undefined
+allTree f = getAll . foldMapTree (All . f)
 
 treeToList :: Tree a -> [a]
-treeToList = undefined
+treeToList = foldMapTree return
+-- if return is forbidden
+--treeToList = foldMapTree (:[])
 
 elemTree :: Eq a => a -> Tree a -> Bool
-elemTree = undefined
+elemTree x = getAny . foldMapTree (Any . (== x))
 
 onMaybe :: (a -> Bool) -> a -> Maybe a
 onMaybe p x = if p x then Just x else Nothing
 
 findPred :: (a -> Bool) -> Tree a -> Maybe a
-findPred = undefined
+findPred p = getFirst . foldMapTree (First . onMaybe p)
 
 findAll :: (a -> Bool) -> Tree a -> [a]
-findAll = undefined
-
+findAll p = foldMapTree (filter p . return)
+-- if return is forbidden
+--findAll p = foldMapTree (filter p . (:[]))
+ 
+-- same as (>>=) for the Maybe monad
 ifJust :: Maybe a -> (a -> Maybe b) -> Maybe b
-ifJust Nothing _ = Nothing
+ifJust Nothing  _ = Nothing
 ifJust (Just x) f = f x
 
 validateTree :: (a -> Maybe b) -> Tree a -> Maybe (Tree b)
-validateTree = undefined
+validateTree _ Empty = Just Empty
+-- coolest
+validateTree f (Node x l r) = do
+  l' <- validateTree f l
+  y  <- f x
+  r' <- validateTree f r
+  Just $ Node y l' r'
+
+-- first idea
+--validateTree f (Node x l r)
+--  = validateTree f l >>= \l' ->
+--    f x >>= \y ->
+--    validateTree f r >>= \r' ->
+--    Just $ Node y l' r'
+
+-- if (>>=) and do are forbidden
+--validateTree f (Node x l r)
+--  = validateTree f l `ifJust` \l' ->
+--    f x `ifJust` \y ->
+--    validateTree f r `ifJust` \r' ->
+--    Just $ Node y l' r'
 
 data Direction
   = L -- go left
@@ -78,7 +133,20 @@ data Direction
   deriving (Show, Eq)
 
 fetch :: [Direction] -> Tree a -> Maybe a
-fetch = undefined
+fetch []     (Node x _ _) = Just x
+fetch (d:ds) (Node _ l r)
+  = fetch ds (case d of L -> l
+                        R -> r)
+fetch _ _ = Nothing
 
 paths :: Tree a -> [(a, [Direction])]
-paths = undefined
+paths = treeToList . mapDirections
+
+-- also in Control.Arrow
+second :: (a -> b) -> (c,a) -> (c,b)
+second f (x,y) = (x, f y)
+
+mapDirections :: Tree a -> Tree (a, [Direction])
+mapDirections = mapTree (second reverse) . go []
+  where go _    Empty        = Empty
+        go path (Node x l r) = Node (x, path) (go (L:path) l) (go (R:path) r)
