0633687d9666c93844fd8036a7d5aae407e78ea1
Deian Goranov <deian.goranov@gmail.com>
2020-02-11 23:01:25 +0200

    Add Datatypes and Program parser

---
+++ project/src/ProgramParser.hs
@@ -0,0 +1,143 @@
+-- TODO: support whitespace
+module ProgramParser where
+
+import Data.Char (isUpper, isLower, isAlphaNum)
+
+import Data.List.NonEmpty (NonEmpty(..), toList)
+
+import DataTypes
+  ( Term(..)
+  , Atom(..)
+  , HornClause(..)
+  , UName
+  , LName
+  , Program
+  )
+
+import Parser
+  ( Parser
+  , parse
+  , nom
+  , result
+  , empty
+  , (<|>)
+  , many
+  , some
+  )
+
+-- Always parses the given character
+char :: Char -> Parser Char
+char c = do
+  x <- nom
+  if x == c
+  then result x
+  else empty
+
+-- Parse the given string, and only the given string
+string :: String -> Parser String
+string []     = result []
+string (c:cs) = do
+  r  <- char c
+  rs <- string cs
+  result $ r:rs
+
+-- Parse a character that satisfies a predicate
+satisfy :: (Char -> Bool) -> Parser Char
+satisfy p = do
+  x <- nom
+  if p x
+  then result x
+  else empty
+
+-- Surround a parser with an opening and closing one
+between :: Parser open -> Parser close -> Parser a -> Parser a
+between open close inside = do
+  open
+  r <- inside
+  close
+  result r
+
+---- A parser that runs both parsers, but ignores the result of the right one.
+---- This is traditionally (<*), and is available for all Applicatives
+--ignoreRight :: Parser a -> Parser b -> Parser a
+--ignoreRight px py = do
+--  x <- px
+--  py
+--  result x
+
+-- Parse a value one or more times, separated by another parser.
+sepBy1 :: Parser sep -> Parser a -> Parser (NonEmpty a)
+sepBy1 sep p = do
+  --xs <- many $ ignoreRight p sep
+  xs <- many $ p <* sep
+  x  <- p
+  result $ case xs of
+             []     -> x :| xs
+             (y:ys) -> y :| ys ++ [x]
+
+--sepBy :: Parser sep -> Parser a -> Parser [a]
+--sepBy sep p = sepBy1 sep p <|> result []
+
+-- Parse a nonempty list of values,
+-- separated by ',' and enclosed by parenthesis,
+-- e.g., parse (vector nom) "(a,b,c,d)" == 'a':|"bcd"
+vector1 :: Parser a -> Parser (NonEmpty a)
+vector1
+  = between (char '(') (char ')')
+  . sepBy1 (char ',')
+
+
+isAlphaNumUnderscore :: Char -> Bool
+isAlphaNumUnderscore c
+  = c == '_' || isAlphaNum c
+
+--------------------------------------------------
+-----      Parsers for program elements      -----
+
+uNameParser :: Parser UName
+uNameParser = do
+  u <- satisfy isUpper
+  rest <- many $ satisfy isAlphaNumUnderscore
+  return $ u:rest
+
+lNameParser :: Parser LName
+lNameParser = do
+  l <- satisfy isLower
+  rest <- many $ satisfy isAlphaNumUnderscore
+  return $ l:rest
+
+varParser :: Parser Term
+varParser = Var <$> uNameParser
+
+constParser :: Parser Term
+constParser = Const <$> lNameParser
+
+funcParser :: Parser Term
+funcParser = Func <$> lNameParser <*> vector1 termParser
+
+termParser :: Parser Term
+termParser = funcParser <|> constParser <|> varParser
+
+atomParser :: Parser Atom
+atomParser = Atom <$> lNameParser <*> vector1 termParser
+
+-- All rules and facts end with '.'
+endOfClause :: Parser ()
+endOfClause = char '.' *> result ()
+
+factParser :: Parser HornClause
+factParser = (:- []) <$> atomParser <* endOfClause
+
+ruleParser :: Parser HornClause
+ruleParser = do
+  a <- atomParser
+  string ":-"
+  ps <- sepBy1 (char ',') atomParser
+  endOfClause
+  return $ a :- toList ps
+
+hornClauseParser :: Parser HornClause
+hornClauseParser = factParser <|> ruleParser
+
+programParser :: Parser Program
+programParser = many hornClauseParser
