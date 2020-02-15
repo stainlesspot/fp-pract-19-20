1fa1f8b4ca2be98c395ea1b757ef00c5e4d299be
Deian Goranov <deian.goranov@gmail.com>
2020-02-15 06:56:15 +0200

    Fix errors in Goal.hs; resolve now works

--- project/src/Prolog/Goal.hs
+++ project/src/Prolog/Goal.hs
@@ -1,9 +1,8 @@
-module Prolog.Goal
-  ( Goal
-  , resolve
-  ) where
+{-# Language TupleSections #-}
 
-import Prolog.Program (Atom, Program)
+module Prolog.Goal (Goal, resolve) where
+
+import Prolog.Program (Atom, HornClause(..), Program)
 import Prolog.Unification
   ( Substitution
   , subAtom
@@ -11,6 +10,7 @@ import Prolog.Unification
   , unifyAtoms
   )
 import Data.List.NonEmpty (NonEmpty(..), toList)
+import Data.Maybe (mapMaybe)
 
 type Goal = [Atom]
 
@@ -24,16 +24,24 @@ resolveAtom prog a = mapMaybe matchClause prog
 
 -- Applies a substitution to a goal.
 subGoal :: Substitution -> Goal -> Goal
-subGoal subst = map $ subAtom substs
+subGoal = map . subAtom
 
+-- Finds all substitutions, possibly infinite, for which the given goal is
+-- resolved from any combination of clauses from the given program.
+-- Every returned substitution, when applied to the input goal,
+-- gives a conjuntion of atoms, which holds true according to the rules in the
+-- given program.
 resolve :: Program -> Goal -> [Substitution]
 resolve _    []     = [[]] -- list of the empty substitution
                            -- because the empty list represents failure
-resolve prog (a:as) = concatMap resolveRest $ resolveAtom prog a
+resolve prog (a:as) = concatMap (uncurry resolveRest) $ resolveAtom prog a
   where
     -- `subst` unifies `a` with some clause in `prog`,
     -- and `bs` is that clause's body.
     -- This means `a` is replaced in the original goal by `bs`.
+    -- `subst` is applied on the new goal,
+    -- as to reflect the unification of `a` with the clause's head.
+    -- `subst` is then composed with every possible 
     resolveRest :: Goal -> Substitution -> [Substitution]
     resolveRest bs subst
       = map (composeSub subst)
